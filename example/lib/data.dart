099ba0046f814dd407c978d9568f2e92e99c3c33
Kenneth Liang <kennethliang0@gmail.com>
2019-05-16 16:22:48 -0400

    Added CupertinoTypeAhead. Updated example project with Cupertino example.

---
+++ example/lib/data.dart
@@ -0,0 +1,37 @@
+import 'dart:math';
+
+class BackendService {
+  static Future<List> getSuggestions(String query) async {
+    await Future.delayed(Duration(seconds: 1));
+
+    return List.generate(3, (index) {
+      return {'name': query + index.toString(), 'price': Random().nextInt(100)};
+    });
+  }
+}
+
+class CitiesService {
+  static final List<String> cities = [
+    'Beirut',
+    'Damascus',
+    'San Fransisco',
+    'Rome',
+    'Los Angeles',
+    'Madrid',
+    'Bali',
+    'Barcelona',
+    'Paris',
+    'Bucharest',
+    'New York City',
+    'Philadelphia',
+    'Sydney',
+  ];
+
+  static List<String> getSuggestions(String query) {
+    List<String> matches = List();
+    matches.addAll(cities);
+
+    matches.retainWhere((s) => s.toLowerCase().contains(query.toLowerCase()));
+    return matches;
+  }
+}
