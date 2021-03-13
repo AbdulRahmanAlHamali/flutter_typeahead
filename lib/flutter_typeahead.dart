eb199a8d9ff8b11170dab0118410e663437d649d
Rafa Ruiz <raruizmu@gmail.com>
2021-03-13 00:31:49 +0000

    Check if the overlay is opened
    Check if the overlay is opened, situations like `WillPopScope` we might want to close just the overlay, instead of the whole `Widget`

--- lib/flutter_typeahead.dart
+++ lib/flutter_typeahead.dart
@@ -1788,6 +1788,10 @@ class SuggestionsBoxController {
   void open() {
     _effectiveFocusNode!.requestFocus();
   }
+    
+  bool isOpened() {
+    return _suggestionsBox!.isOpened;
+  }
 
   /// Closes the suggestions box
   void close() {
