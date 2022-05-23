e623d468ce2c9f2ab60485ca9752e9b1a54a19fc
dab246 <tdvu@linagora.com>
2022-05-23 17:05:21 +0700

    Create widget typeahead for quick search

--- lib/flutter_typeahead.dart
+++ lib/flutter_typeahead.dart
@@ -3,3 +3,4 @@ library flutter_typeahead;
 export 'src/typedef.dart';
 export 'src/flutter_typeahead.dart';
 export 'src/cupertino_flutter_typeahead.dart';
+export 'src/flutter_typeahead_for_quick_search.dart';
--- lib/src/typedef.dart
+++ lib/src/typedef.dart
@@ -3,8 +3,10 @@ import 'dart:async';
 import 'package:flutter/widgets.dart';
 
 typedef FutureOr<Iterable<T>> SuggestionsCallback<T>(String pattern);
+typedef FutureOr<Iterable<R>> GetRecentCallback<R>(String pattern);
 typedef Widget ItemBuilder<T>(BuildContext context, T itemData);
 typedef void SuggestionSelectionCallback<T>(T suggestion);
+typedef void RecentSelectionCallback<R>(R recent);
 typedef Widget ErrorBuilder(BuildContext context, Object? error);
 
 typedef Widget AnimationTransitionBuilder(
