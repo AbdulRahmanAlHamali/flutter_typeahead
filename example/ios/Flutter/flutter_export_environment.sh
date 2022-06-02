5d4b52c2d5c0b3dfb4e27a01915a488bf9a21614
nathanielajayi7 <nathanielajayi646@gmail.com>
2022-06-02 03:49:09 -0700

    null safety widgets instance

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/sjm/Development/tools/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/sjm/play/master/flutter_typeahead/example"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Nathan#\Desktop\myForks\flutter_typeahead\example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
