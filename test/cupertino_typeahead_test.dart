5bb8f91693ae26fe80fbc639ba3baeda5039fc8a
Eivind Frøystad <69076939+eivihnd@users.noreply.github.com>
2023-07-13 12:27:19 +0200

    fix: remove redundant import

--- test/cupertino_typeahead_test.dart
+++ test/cupertino_typeahead_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'helpers/cupertino_typeahead_helper.dart';
