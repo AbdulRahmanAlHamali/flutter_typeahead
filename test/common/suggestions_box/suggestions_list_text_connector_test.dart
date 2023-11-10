d1e8adea0facb9f023279f7ecf5e6a5e36de4f2b
dragon <clragon@clynamic.net>
2023-11-10 20:12:29 +0100

    fix: tap connector

--- test/common/suggestions_box/suggestions_list_text_connector_test.dart
+++ test/common/suggestions_box/suggestions_list_text_connector_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
-import 'package:flutter_typeahead/src/common/suggestions_box/suggestions_list_reopen_connector.dart';
+import 'package:flutter_typeahead/src/common/suggestions_box/suggestions_list_text_connector.dart';
 
 void main() {
   group('SuggestionsListTextConnector', () {
@@ -27,7 +27,7 @@ void main() {
       await tester.pumpWidget(
         MaterialApp(
           home: Material(
-            child: SuggestionsListReopenConnector(
+            child: SuggestionsListTextConnector(
               controller: controller,
               textEditingController: textEditingController,
               child: const SizedBox(),
@@ -42,62 +42,6 @@ void main() {
       await tester.pump();
 
       expect(controller.isOpen, isTrue);
-    });
-
-    testWidgets(
-        'opens the suggestions box when tapped if it is not open and retainFocus is true',
-        (WidgetTester tester) async {
-      controller.open();
-      controller.close(retainFocus: true);
-
-      await tester.pumpWidget(
-        MaterialApp(
-          home: Material(
-            child: SuggestionsListReopenConnector(
-              controller: controller,
-              textEditingController: textEditingController,
-              child: InkWell(
-                onTap: () {},
-              ),
-            ),
-          ),
-        ),
-      );
-
-      expect(controller.isOpen, isFalse);
-
-      await tester.tap(find.byType(InkWell));
-      await tester.pump();
-
-      expect(controller.isOpen, isTrue);
-    });
-
-    testWidgets(
-        'does not open the suggestions box when tapped if it is not open and retainFocus is false',
-        (WidgetTester tester) async {
-      controller.open();
-      controller.close(retainFocus: false);
-
-      await tester.pumpWidget(
-        MaterialApp(
-          home: Material(
-            child: SuggestionsListReopenConnector(
-              controller: controller,
-              textEditingController: textEditingController,
-              child: InkWell(
-                onTap: () {},
-              ),
-            ),
-          ),
-        ),
-      );
-
-      expect(controller.isOpen, isFalse);
-
-      await tester.tap(find.byType(InkWell));
-      await tester.pump();
-
-      expect(controller.isOpen, isFalse);
     });
   });
 }
