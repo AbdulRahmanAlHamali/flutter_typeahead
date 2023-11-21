562445c6334ee36ac87a889d0f05d4f37f15c278
dragon <clragon@clynamic.net>
2023-11-21 16:21:03 +0100

    chore: rename files

--- lib/flutter_typeahead.dart
+++ lib/flutter_typeahead.dart
@@ -3,4 +3,4 @@ library flutter_typeahead;
 export 'package:flutter_typeahead/src/common/base/suggestions_controller.dart';
 export 'package:flutter_typeahead/src/common/field/typeahead_field.dart';
 export 'package:flutter_typeahead/src/material/typeahead_field.dart';
-export 'package:flutter_typeahead/src/cupertino/cupertino_typeahead_field.dart';
+export 'package:flutter_typeahead/src/cupertino/typeahead_field.dart';
