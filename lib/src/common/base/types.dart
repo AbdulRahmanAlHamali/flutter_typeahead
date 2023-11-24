fe0ab788a5f78dd0a00565953fe1d8a19b696d79
dragon <clragon@clynamic.net>
2023-11-24 15:39:00 +0100

    test: suggestions list

--- lib/src/common/base/types.dart
+++ lib/src/common/base/types.dart
@@ -5,7 +5,7 @@ import 'package:flutter/widgets.dart';
 typedef SuggestionsCallback<T> = FutureOr<List<T>> Function(String pattern);
 typedef ItemBuilder<T> = Widget Function(BuildContext context, T value);
 typedef SuggestionSelectionCallback<T> = void Function(T suggestion);
-typedef ErrorBuilder = Widget Function(BuildContext context, Object? error);
+typedef ErrorBuilder = Widget Function(BuildContext context, Object error);
 typedef TextFieldBuilder = Widget Function(
   BuildContext context,
   TextEditingController controller,
---
+++ test/common/box/suggestions_list_test.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_typeahead/src/common/base/types.dart';
+import 'package:flutter_typeahead/src/common/box/suggestions_list.dart';
+import 'package:flutter_typeahead/src/common/base/suggestions_controller.dart';
+
+void main() {
+  group('SuggestionsList', () {
+    late SuggestionsController<String> controller;
+    late List<String> suggestions;
+    late ItemBuilder<String> itemBuilder;
+    late WidgetBuilder loadingBuilder;
+    late ErrorBuilder errorBuilder;
+    late WidgetBuilder emptyBuilder;
+
+    setUp(() {
+      controller = SuggestionsController();
+      suggestions = ['test1', 'test2', 'test3'];
+      itemBuilder =
+          (context, suggestion) => Text(key: Key(suggestion), suggestion);
+      loadingBuilder =
+          (context) => const Text(key: Key('loading'), 'Loading...');
+      errorBuilder =
+          (context, error) => Text(key: const Key('error'), '$error');
+      emptyBuilder =
+          (context) => const Text(key: Key('empty'), 'No suggestions');
+    });
+
+    tearDown(() {
+      controller.dispose();
+    });
+
+    testWidgets('shows loading builder when isLoading is true',
+        (WidgetTester tester) async {
+      controller.isLoading = true;
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Material(
+            child: SuggestionsList(
+              controller: controller,
+              itemBuilder: itemBuilder,
+              loadingBuilder: loadingBuilder,
+              errorBuilder: errorBuilder,
+              emptyBuilder: emptyBuilder,
+            ),
+          ),
+        ),
+      );
+
+      expect(find.byKey(const Key('loading')), findsOneWidget);
+    });
+
+    testWidgets('shows error builder when isError is true',
+        (WidgetTester tester) async {
+      controller.error = 'An error occurred';
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Material(
+            child: SuggestionsList(
+              controller: controller,
+              itemBuilder: itemBuilder,
+              loadingBuilder: loadingBuilder,
+              errorBuilder: errorBuilder,
+              emptyBuilder: emptyBuilder,
+            ),
+          ),
+        ),
+      );
+
+      expect(find.byKey(const Key('error')), findsOneWidget);
+    });
+
+    testWidgets('shows empty builder when suggestions list is empty',
+        (WidgetTester tester) async {
+      controller.suggestions = [];
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Material(
+            child: SuggestionsList(
+              controller: controller,
+              itemBuilder: itemBuilder,
+              loadingBuilder: loadingBuilder,
+              errorBuilder: errorBuilder,
+              emptyBuilder: emptyBuilder,
+            ),
+          ),
+        ),
+      );
+
+      expect(find.byKey(const Key('empty')), findsOneWidget);
+    });
+
+    testWidgets('shows list of suggestions when suggestions list is not empty',
+        (WidgetTester tester) async {
+      controller.suggestions = suggestions;
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Material(
+            child: SuggestionsList(
+              controller: controller,
+              itemBuilder: itemBuilder,
+              loadingBuilder: loadingBuilder,
+              errorBuilder: errorBuilder,
+              emptyBuilder: emptyBuilder,
+            ),
+          ),
+        ),
+      );
+
+      expect(find.byKey(const Key('test1')), findsOneWidget);
+      expect(find.byKey(const Key('test2')), findsOneWidget);
+      expect(find.byKey(const Key('test3')), findsOneWidget);
+    });
+
+    testWidgets(
+        'reverses the suggestions list based on effective controller direction',
+        (WidgetTester tester) async {
+      controller.suggestions = suggestions;
+      controller.effectiveDirection = VerticalDirection.up;
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Material(
+            child: SuggestionsList(
+              controller: controller,
+              itemBuilder: itemBuilder,
+              loadingBuilder: loadingBuilder,
+              errorBuilder: errorBuilder,
+              emptyBuilder: emptyBuilder,
+              autoFlipListDirection: true,
+            ),
+          ),
+        ),
+      );
+
+      final list = tester.widget<ListView>(find.byType(ListView));
+      expect(list.reverse, true);
+    });
+
+    testWidgets('uses custom list builder', (WidgetTester tester) async {
+      controller.suggestions = suggestions;
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Material(
+            child: SuggestionsList(
+              controller: controller,
+              itemBuilder: itemBuilder,
+              loadingBuilder: loadingBuilder,
+              errorBuilder: errorBuilder,
+              emptyBuilder: emptyBuilder,
+              listBuilder: (context, children) => GridView.count(
+                crossAxisCount: 2,
+                children: children,
+              ),
+            ),
+          ),
+        ),
+      );
+
+      expect(find.byType(GridView), findsOneWidget);
+    });
+  });
+}
